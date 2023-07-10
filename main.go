8f8e28e44e055311981997945d2e8fbaaa5408de
Mikhail Pchelnikov <envy2f@mail.ru>
2023-07-10 16:40:57 -0400

    ops set added

--- main.go
+++ main.go
@@ -8,19 +8,49 @@ import (
 	//"strings"
 )
 
+
+
+func checkOpS(testStrKey string) bool {
+
+
+	type void struct {} //  sets helpers
+	var voidEl void		//	sets helpers
+	OpSset := make(map([string]void)
+
+	OpSset["+"] = voidEl
+	OpSset["-"] = voidEl
+	OpSset["/"] = voidEl
+	OpSset["*"] = voidEl
+
+	if _,ok := OpSset[testStrKey]; ok {
+		return true
+	} else {
+		return false
+	}
+}
+
+
 func main() {
 	//reader := bufio.NewReader(os.Stdin)
+
+
+
+
+
+	}
+
 	var str1, str2, str3 string
 	for {
+		fmt.Println("Будет выведен результат, если введете \" два операнда и один оператор между ними (через пробелы) \"")
 		fmt.Print("Enter text: ")
 		//text, _ := reader.ReadString('\n')
 		fmt.Scanln(&str1, &str2, &str3)
-		if str1 != "" && str2 != "" && str3 != "" {
+		if str1 != "" && str2 != "" && str3 != ""  {
 
 			fmt.Println(str1, str2, str3)
 
 		} else {
-			fmt.Println("вводите задания в формате: \" два операнда и один оператор между ними (через пробел)\"")
+			fmt.Println("Ваша строка не соотвествует формату: \" два операнда и один оператор между ними (через пробелы) \"")
 			break
 		} //if
 
