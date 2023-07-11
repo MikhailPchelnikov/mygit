3756c67cdaa66135b255dfc651dc6b3aed2853df
Mikhail Pchelnikov <envy2f@mail.ru>
2023-07-11 10:16:22 -0400

    errormsg added

--- main.go
+++ main.go
@@ -8,54 +8,115 @@ import (
 	//"strings"
 )
 
-
-
 func checkOpS(testStrKey string) bool {
 
+	type void struct{} //  sets helpers
+	var voidEl void    //	sets helpers
+	OpsSet := make(map[string]void)
 
-	type void struct {} //  sets helpers
-	var voidEl void		//	sets helpers
-	OpSset := make(map([string]void)
+	OpsSet["+"] = voidEl
+	OpsSet["-"] = voidEl
+	OpsSet["/"] = voidEl
+	OpsSet["*"] = voidEl
 
-	OpSset["+"] = voidEl
-	OpSset["-"] = voidEl
-	OpSset["/"] = voidEl
-	OpSset["*"] = voidEl
-
-	if _,ok := OpSset[testStrKey]; ok {
+	if _, ok := OpsSet[testStrKey]; ok {
 		return true
 	} else {
 		return false
 	}
 }
 
+func checkOperandsArab(testStrKey string) bool {
+
+	type void struct{} //  sets helpers
+	var voidEl void    //	sets helpers
+	ArabSet := make(map[string]void)
+
+	ArabSet["0"] = voidEl
+	ArabSet["1"] = voidEl
+	ArabSet["2"] = voidEl
+	ArabSet["3"] = voidEl
+	ArabSet["4"] = voidEl
+	ArabSet["5"] = voidEl
+	ArabSet["6"] = voidEl
+	ArabSet["7"] = voidEl
+	ArabSet["8"] = voidEl
+	ArabSet["9"] = voidEl
+	ArabSet["10"] = voidEl
+	if _, ok := ArabSet[testStrKey]; ok {
+		return true
+	} else {
+		return false
+	}
+}
+
+func checkOperandsRome(testStrKey string) bool {
+
+	type void struct{} //  sets helpers
+	var voidEl void    //	sets helpers
+	RomeSet := make(map[string]void)
+
+	RomeSet["I"] = voidEl
+	RomeSet["II"] = voidEl
+	RomeSet["III"] = voidEl
+	RomeSet["IV"] = voidEl
+	RomeSet["V"] = voidEl
+	RomeSet["VI"] = voidEl
+	RomeSet["VII"] = voidEl
+	RomeSet["VIII"] = voidEl
+	RomeSet["IX"] = voidEl
+	RomeSet["X"] = voidEl
+
+	if _, ok := RomeSet[testStrKey]; ok {
+		return true
+	} else {
+		return false
+	}
+}
+
+const BasicErrorMsg = "Ваша строка не соотвествует формату: \" два операнда и один оператор между ними (через пробелы) \""
 
 func main() {
-	//reader := bufio.NewReader(os.Stdin)
+	RomeMap := make(map[string]int)
 
+	RomeMap["I"] = 1
+	RomeMap["II"] = 2
+	RomeMap["III"] = 3
+	RomeMap["IV"] = 4
+	RomeMap["V"] = 5
+	RomeMap["VI"] = 6
+	RomeMap["VII"] = 7
+	RomeMap["VIII"] = 8
+	RomeMap["IX"] = 9
+	RomeMap["X"] = 10
 
-
-
-
-	}
-
-	var str1, str2, str3 string
+	//reader := bufio.NewReader(os.Stdin)s
+	var str1, str2, str3, str4, errorString string
+	str4 = "cheatcode543210string" //uniq string life user without special knowledge never enters
+	errorString = BasicErrorMsg
 	for {
+
 		fmt.Println("Будет выведен результат, если введете \" два операнда и один оператор между ними (через пробелы) \"")
-		fmt.Print("Enter text: ")
+		//fmt.Print("Enter text: ")
 		//text, _ := reader.ReadString('\n')
-		fmt.Scanln(&str1, &str2, &str3)
-		if str1 != "" && str2 != "" && str3 != ""  {
+		fmt.Scanln(&str1, &str2, &str3, &str4)
+		if str4 != "cheatcode543210string" {
+			errorString = ("Учтите, Вы ввели слишком много. " + BasicErrorMsg)
+		}
+
+		if str1 != "" && str2 != "" && str3 != "" && checkOpS(str2) {
 
 			fmt.Println(str1, str2, str3)
 
 		} else {
-			fmt.Println("Ваша строка не соотвествует формату: \" два операнда и один оператор между ними (через пробелы) \"")
+			//fmt.Println(errorstr)
 			break
 		} //if
 
 		str1 = ""
 		str2 = ""
 		str3 = ""
+		//str4 = "cheatcode543210string" //uniq string life user without special knowledge never enters
 	} //for
+	fmt.Println(errorString)
 } //main
